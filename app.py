309afca457183e5c10701e992790d2da7e4d0c22
Andreas Domberg <andom@posteo.de>
2024-03-26 15:12:57 +0100

    Cards work. Need to deal with dashapp template, use render_template().

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
+import base64
 import os
 from flask import Flask, render_template
+
 import dashapps.interface_dashapp1
 import dashapps.interface_dashapp2
 import dashapps.interface_pks
+
 # from dashapps.dashapp1 import dashapp1_init
+
 
 flask_app = Flask(__name__)
 
 dashapps.interface_pks.init(flask_app)
 dashapps.interface_dashapp1.init(flask_app)
 dashapps.interface_dashapp2.init(flask_app)
+
 
 @flask_app.route("/")
 def index():
@@ -21,6 +26,14 @@ def index():
 
     posts = []
 
+    def read_png_file(file_path):
+        if file_path is None:
+            return None
+
+        with open(file_path, "rb") as file:
+            png_content = base64.b64encode(file.read()).decode("utf-8")
+        return png_content
+
     # Iterate through interface files
     for module_name in dir(dashapps):
         if module_name.startswith("interface_"):
@@ -28,11 +41,24 @@ def index():
             if hasattr(module, "metadata"):
                 metadata = module.metadata
                 route = metadata.get("route")
+                thumbnailpath = metadata.get("thumbnailpath")
+
+                if thumbnailpath:
+                    metadata["thumbnail"] = read_png_file(
+                        "dashapps"
+                        + metadata.get("route", "")
+                        + metadata.get("thumbnailpath", "")
+                    )
+                else:
+                    metadata["thumbnail"] = read_png_file("static/none.png")
+
                 if route:
                     posts += [metadata]
 
-    for post in posts:
-        print(post.get("route", "(route)") + post.get("thumbnail", "(thumb)"))
+    # add a small random rotation and side shift to each post:
+    for i, post in enumerate(posts):
+        post["rotation"] = (2*(i % 2) - 1) * 3
+        post["offset"] = [2, 5, 1, 4, 3][i % 5]
 
     return render_template(
         "index.html",
--- dashapps/dashapp1/metadata/__init__.py
+++ dashapps/dashapp1/metadata/__init__.py
@@ -1,5 +1,4 @@
 metadata = {
     "title": "Dash App Uno",
     "route": "/dashapp1/",
-    "synopsis": "This is a Dash app that demonstrates the use of the Dash framework.",
 }
