2fbc1c2e27982b7efc0141480f099a4fbdb6276f
Andreas Domberg <andom@posteo.de>
2024-03-29 22:44:11 +0100

    dash pages work with template

--- app.py
+++ app.py
@@ -5,24 +5,30 @@ from flask import Flask, render_template
 import dashapps.interface_dashapp1
 import dashapps.interface_dashapp2
 import dashapps.interface_pks
-
-# from dashapps.dashapp1 import dashapp1_init
+from dashapps.pks.pks import init_dashboard
+from dashapps.pks.metadata import metadata as metadata_pks
 
 
 flask_app = Flask(__name__)
 
-dashapps.interface_pks.init(flask_app)
 dashapps.interface_dashapp1.init(flask_app)
 dashapps.interface_dashapp2.init(flask_app)
+pksapp = init_dashboard(flask_app, "/pks/")
 
+nav_entries = {
+    "Home": "/",
+    "Contact": "/contact",
+}
+
+with flask_app.test_request_context("/?name=test"):
+    pksapp.index_string = render_template(
+        "dashapp.html",
+        nav_entries=nav_entries,
+        title="Polizeiliche Kriminalstatistik"
+    )
 
 @flask_app.route("/")
 def index():
-
-    nav_entries = {
-        "Home": "/",
-        "Contact": "/contact",
-    }
 
     posts = []
 
@@ -41,13 +47,13 @@ def index():
             if hasattr(module, "metadata"):
                 metadata = module.metadata
                 route = metadata.get("route")
-                thumbnailpath = metadata.get("thumbnailpath")
+                thumbnailpath = metadata.get("thumbnail")
 
                 if thumbnailpath:
                     metadata["thumbnail"] = read_png_file(
                         "dashapps"
                         + metadata.get("route", "")
-                        + metadata.get("thumbnailpath", "")
+                        + metadata.get("thumbnail", "")
                     )
                 else:
                     metadata["thumbnail"] = read_png_file("static/none.png")
@@ -55,7 +61,7 @@ def index():
                 if route:
                     posts += [metadata]
 
-    # add a small random rotation and side shift to each post:
+    # add a small rotation and side shift to each post:
     for i, post in enumerate(posts):
         post["rotation"] = (2*(i % 2) - 1) * 3
         post["offset"] = [2, 5, 1, 4, 3][i % 5]
