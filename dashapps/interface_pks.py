bcf2ed7e622d907d15ef9542cb8665fb6c0ab9f2
Andreas Domberg <andom@posteo.de>
2024-03-30 11:50:59 +0100

    clean up interfaces

--- dashapps/interface_pks.py
+++
@@ -1,8 +0,0 @@
-from .pks.pks import init_dashboard
-from .pks.metadata import metadata
-from src.with_metadata import with_metadata
-
-
-@with_metadata(title=metadata.get("title", "Default title"), route=metadata.get("route", "/default/"))
-def init(flask_app, route):
-    return init_dashboard(flask_app, route)
