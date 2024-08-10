33490d7639e8588cdc45280e31aa56921dfdbfbf
Andreas Domberg <andom@posteo.de>
2024-08-10 16:26:32 +0200

    remove old submodules

--- dashapps/dashapp1/dashboard.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from . import dashapp1_init
-
-
-app = Flask(__name__, instance_relative_config=False)
-app = dashapp1_init(app, route="/")
-app.run(host="0.0.0.0", port=8080, debug=True, load_dotenv=False)
--- dashapps/dashapp2/dashboard.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from . import dashapp2_init
-
-
-app = Flask(__name__, instance_relative_config=False)
-app = dashapp2_init(app, route="/")
-app.run(host="0.0.0.0", port=8080, debug=True, load_dotenv=False)
