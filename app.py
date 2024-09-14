e058bd4a479db43320c5a42c783fb8d30d2c4d63
Andreas Domberg <andom@posteo.de>
2024-09-14 22:46:54 +0200

    amend to last commit

--- app.py
+++ app.py
@@ -135,6 +135,7 @@ def create_app():
     return app
 
 
+flask_app = create_app()
+
 if __name__ == "__main__":
-    flask_app = create_app()
     flask_app.run(host="0.0.0.0", port=5000, debug=True)
