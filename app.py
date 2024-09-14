aef585b25aa95a0b83480bd40300a045981d2852
Andreas Domberg <andom@posteo.de>
2024-09-14 22:45:46 +0200

    finally fix the fact that the app object had a different name (app) than expected by wsgi (flask_app)!

--- app.py
+++ app.py
@@ -136,5 +136,5 @@ def create_app():
 
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    flask_app = create_app()
+    flask_app.run(host="0.0.0.0", port=5000, debug=True)
